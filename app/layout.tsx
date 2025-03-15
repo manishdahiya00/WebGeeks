00986c4f6fa83fa5374ab28a66e68487e52ab4c3
Manish Dahiya <manishdahiya0944@gmail.com>
2025-03-15 15:56:59 +0530

    minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import { ThemeProvider } from "@/components/layout/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shadcn - Landing template",
-  description: "Landing template from Shadcn",
+  title: "WebGeeks - Software Development Company",
+  description:
+    "Webgeeks is a software development company. It includes software development, web design, and web development. We are a group of developers who are passionate about creating software that is user-friendly and easy to use. For more information please visit https://webgeeks-in.vercel.app",
 };
 
 export default function RootLayout({
